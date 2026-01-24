dc04f1834e55b8d8301fb848d0e9ec07803c5988
NEWTOWNBOY <38236367+newtownboy@users.noreply.github.com>
2026-01-24 11:56:00 +0900

    [level 1] Title: 폰켓몬, Time: 0.73 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -1,7 +1,6 @@
-from itertools import combinations
-
 def solution(nums):
-    kinds = set(nums)
-    max_selectable = len(nums) // 2
-    
-    return min(len(kinds), max_selectable)
+    # 서로 다른 종류의 개수
+    unique = len(set(nums))
+    # 뽑을 수 있는 최대 개수 = 전체 길이의 절반
+    limit = len(nums) // 2
+    return min(unique, limit)
