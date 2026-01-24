ae069e55aed6fded872bd41aa95f0eb2ad56b61c
NEWTOWNBOY <38236367+newtownboy@users.noreply.github.com>
2026-01-24 12:31:42 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 48.78 ms, Memory: 33.1 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/README.md
+++ 프로그래머스/1/42576. 완주하지 못한 선수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 완주하지 못한 선수 - 42576 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576) 
 
 ### 성능 요약
 
-메모리: 38.1 MB, 시간: 65.03 ms
+메모리: 33.1 MB, 시간: 48.78 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 01월 24일 11:39:35
+2026년 01월 24일 12:31:38
 
 ### 문제 설명
 
--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,8 +1,15 @@
-from collections import Counter
-
+# 1. 참가자 배열을 순회하면서 이름은 key, 등장 횟수를 value로 저장
+# 2. 완주자 배열을 순회하면서 해당 이름의 value를 하나씩 줄인다.
+# 3. 마지막에 value가 1이상 남아있는 key가 완주하지 못한 선수
 def solution(participant, completion):
-    counterP = Counter(participant)
-    counterC = Counter(completion)
-    diff = counterP - counterC
+    dic = {}
     
-    return list(diff.elements())[0]
+    for p in participant:
+        dic[p] = dic.get(p, 0) + 1
+        
+    for c in completion:
+        dic[c] -= 1
+    
+    for k, v in dic.items():
+        if v > 0:
+            return k
