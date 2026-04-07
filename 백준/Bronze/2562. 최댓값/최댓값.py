5d9cdb9c228c89421bf2aad6f6ca31b4f0202a71
NEWTOWNBOY <38236367+newtownboy@users.noreply.github.com>
2026-04-07 17:15:46 +0900

    [Bronze III] Title: 최댓값, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.py
+++ 백준/Bronze/2562. 최댓값/최댓값.py
@@ -1,26 +1,16 @@
-# 9개의 서로 다른 자연수가 주어질 때, 이들 중 최댓값을 찾고 그 최댓값이 몇 번째 수인지를 구하는 프로그램을 작성하시오.
-# 예를 들어, 서로 다른 9개의 자연수
-# 3, 29, 38, 12, 57, 74, 40, 85, 61
-# 이 주어지면, 이들 중 최댓값은 85이고, 이 값은 8번째 수이다.
-
 def solve() -> None:
-    before_value = 0
     max_value = 0
     max_pos = 0
 
-    n = 9
-
-    for i in range(n):
-        before_value = max_value
-
+    for i in range(9):
         num = int(input())
-        max_value = max(max_value, num)
-
-        if before_value != max_value:
+        if num > max_value:
+            max_value = num
             max_pos = i + 1
 
     print(max_value)
     print(max_pos)
+
 
 def main() -> None:
     solve()
